4f8a495b832f08de4478ccd5f55253cccef5136a
itsoluwatobby <itsoluwatobby@gmail.com>
2023-07-25 22:47:18 +0100

    m

---
+++ malloc.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+#include <stdlib.h>
+
+/**
+ * _malloc - allocates memory to a variable
+ * @size: variable size
+ * Return: pointer to memory
+ */
+
+void* _malloc(int size)
+{
+	void* p = NULL;
+
+	p = malloc(sizeof(char) * (size + 1));
+	if (p == NULL)
+	{
+		print_string("Memory allocation failed. Exiting...\n");
+		exit(1);
+	}
+
+	return (p);
+}
